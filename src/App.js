e5011858b760b907cf556273776a93d86fa8b21c
Kirsten Naughton <kirsten.naughton@gmail.com>
2019-03-01 17:11:59 -0700

    practicing components with dynamic elements

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'; //lowercase is reserved for jsx, so custom components are upper case
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <h1>jsx is javascript that gets compiled to createElement's</h1>
+        <p>This is a paragraph</p>
+        <Person name="James" age="35" hobbies="sky diving" /> 
+        <Person name="Johnson" age="75"/> 
+        <Person name="Harrison" age="20"/> 
+        <Person name="Ringo" age="101"/> 
+        <Person name="Starr" age="36">My hobbies are: RACING!!</Person> 
       </div>
     );
+
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Is this working?'));
+    //this createElement is the same as using JSX but much harder so we just use JSX
   }
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const person = (props) => {
+    return (
+        <div>
+        <p>I'm {props.name} and I am {props.age} years old! {props.hobbies}, baby!</p>
+        <p>{props.children}</p>
+        </div>
+        );
+};
+
+//children is a reserved word.
+
+//execute one line expressions. can use functions if we need more complex information
+
+//this is a function that returns some jsx
+//also have to do the imports and exports
+
+export default person;
+
+
+/// components are awesome because they are small and maintainable, configurable, and reusable
+
+//should be dynamic depending on user input
